57f2cb38bc08581194615f1e7e35539302703b15
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-04-10 11:54:43 +0100

    basic skeleton for dashboard layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import './Sidebar.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBoltLightning, faBullhorn, faClone, } from '@fortawesome/free-solid-svg-icons'
 
 function Sidebar() {
+    const [showSidebar, setShowSidebar] = React.useState(false)
+    const [sidebarContent, setSidebarContent] = React.useState('')
+
+    const handleButtonClick = (type: string | '') => {
+        if (type === sidebarContent) {
+            setShowSidebar(false)
+            setSidebarContent('')
+        }
+        if (type !== sidebarContent) {
+            setShowSidebar(true)
+            setSidebarContent(type)
+        }
+    }
+
+
     return (
-        <div>Sidebar</div>
+        <div className='sidebar'>
+            <button onClick={() => handleButtonClick('Templates')}>
+                <FontAwesomeIcon icon={faClone} />
+            </button>
+            <button onClick={() => handleButtonClick('Campaigns')}>
+                <FontAwesomeIcon icon={faBullhorn} />
+
+            </button>
+            <button onClick={() => handleButtonClick('Tasks')}>
+                <FontAwesomeIcon icon={faBoltLightning} />
+
+            </button>
+            {
+                showSidebar && (
+                    <div className={`sidebar-content ${showSidebar ? 'showing' : ''}`}>
+                        <h4>{sidebarContent}</h4>
+                    </div>
+                )
+            }
+        </div>
     )
 }
 
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import './DashboardLayout.scss'
 
 function DashboardLayout(props: { sidebar: React.ReactNode, outlet: React.ReactNode }) {
 
     const { sidebar: Sidebar, outlet: Outlet } = props
 
     return (
-        <div className='dahboard-layout'>
-            <div className='sidebar'>{Sidebar}</div>
-            <div className='outlet'>{Outlet}</div>
+        <div className='dashboard-layout'>
+            <div className='sidebar-layout'>{Sidebar}</div>
+            <div className='dashboard-outlet'>{Outlet}</div>
         </div>
     )
 }
